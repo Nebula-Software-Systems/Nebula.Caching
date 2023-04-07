e3f71f900adf087688280aa993936002e4013f33
Rafael Camara <rafaelcamarac@gmail.com>
2023-04-07 17:03:51 +0200

    Remove method dependency injection.

--- src/Redis/Interceptors/RedisCacheInterceptor.cs
+++ src/Redis/Interceptors/RedisCacheInterceptor.cs
@@ -19,6 +19,9 @@ namespace Nebula.Caching.Redis.Interceptors
         private ICacheManager _cacheManager { get; set; }
         private IKeyManager _keyManager { get; set; }
         private IContextUtils _utils { get; set; }
+        private AspectContext context { get; set; }
+        private AspectDelegate next { get; set; }
+
 
         public RedisCacheInterceptor(ICacheManager cacheManager, IKeyManager keyManager, IContextUtils utils)
         {
@@ -29,36 +32,38 @@ namespace Nebula.Caching.Redis.Interceptors
 
         public async override Task Invoke(AspectContext context, AspectDelegate next)
         {
-            if (ExecutedMethodHasRedisCacheAttribute(context)) await ExecuteMethodThatHasRedisCacheAttribute(context, next).ConfigureAwait(false);
-            else await ContinueExecutionForNonCacheableMethod(context, next).ConfigureAwait(false);
+            this.context = context;
+            this.next = next;
+            if (ExecutedMethodHasRedisCacheAttribute()) await ExecuteMethodThatHasRedisCacheAttribute().ConfigureAwait(false);
+            else await ContinueExecutionForNonCacheableMethod().ConfigureAwait(false);
         }
 
-        private bool ExecutedMethodHasRedisCacheAttribute(AspectContext context)
+        private bool ExecutedMethodHasRedisCacheAttribute()
         {
             return _utils.IsAttributeOfType<RedisCacheAttribute>(context);
         }
 
-        private async Task ExecuteMethodThatHasRedisCacheAttribute(AspectContext context, AspectDelegate next)
+        private async Task ExecuteMethodThatHasRedisCacheAttribute()
         {
-            if (await CacheExistsAsync(context).ConfigureAwait(false))
+            if (await CacheExistsAsync().ConfigureAwait(false))
             {
-                await ReturnCachedValueAsync(context).ConfigureAwait(false);
+                await ReturnCachedValueAsync().ConfigureAwait(false);
             }
             else
             {
                 await next(context).ConfigureAwait(false);
-                await CacheValueAsync(context).ConfigureAwait(false);
+                await CacheValueAsync().ConfigureAwait(false);
             }
         }
 
-        private async Task ContinueExecutionForNonCacheableMethod(AspectContext context, AspectDelegate next)
+        private async Task ContinueExecutionForNonCacheableMethod()
         {
             await next(context).ConfigureAwait(false);
         }
 
-        private async Task ReturnCachedValueAsync(AspectContext context)
+        private async Task ReturnCachedValueAsync()
         {
-            var value = await _cacheManager.GetAsync(GenerateKey(context)).ConfigureAwait(false);
+            var value = await _cacheManager.GetAsync(GenerateKey()).ConfigureAwait(false);
 
             if (context.IsAsync())
             {
@@ -73,7 +78,7 @@ namespace Nebula.Caching.Redis.Interceptors
             }
         }
 
-        private async Task CacheValueAsync(AspectContext context)
+        private async Task CacheValueAsync()
         {
             string value = "";
 
@@ -88,18 +93,18 @@ namespace Nebula.Caching.Redis.Interceptors
                 value = JsonConvert.SerializeObject(returnValue);
             }
 
-            var key = GenerateKey(context);
-            var expiration = TimeSpan.FromSeconds(_utils.GetCacheDuration<RedisCacheAttribute>(GenerateKey(context), context));
+            var key = GenerateKey();
+            var expiration = TimeSpan.FromSeconds(_utils.GetCacheDuration<RedisCacheAttribute>(GenerateKey(), context));
 
             await _cacheManager.SetAsync(key, value, expiration).ConfigureAwait(false);
         }
 
-        private async Task<bool> CacheExistsAsync(AspectContext context)
+        private async Task<bool> CacheExistsAsync()
         {
-            return await _cacheManager.CacheExistsAsync(GenerateKey(context)).ConfigureAwait(false);
+            return await _cacheManager.CacheExistsAsync(GenerateKey()).ConfigureAwait(false);
         }
 
-        private string GenerateKey(AspectContext context)
+        private string GenerateKey()
         {
             return _keyManager.GenerateKey(_utils.GetExecutedMethodInfo(context), _utils.GetMethodParameters(context));
         }
