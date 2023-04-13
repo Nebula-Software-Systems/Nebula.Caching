d8eb30100feb076f2ae4b2e8198cc4eaf6321267
Rafael Camara <rafaelcamarac@gmail.com>
2023-04-13 17:43:49 +0200

    Add tests to RedisCacheManager class.

--- tests/Redis/CacheManager/RedisCacheManagerTests.cs
+++ tests/Redis/CacheManager/RedisCacheManagerTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using Moq;
 using Nebula.Caching.Redis.CacheManager;
 using StackExchange.Redis;
@@ -23,7 +24,6 @@ namespace Nebula.Caching.tests.Redis.CacheManager
             Assert.Equal(expectedCacheExistence, cacheExists);
             mockedIDatabase.Verify(m => m.StringGet("key", It.IsAny<CommandFlags>()), Times.Once);
         }
-
 
         [Fact]
         public void Given_AMethodExecutedWithRedisCacheAttribute_When_CacheDoesNotExist_Then_ReturnFalseBecauseCacheDoesNotExist()
@@ -59,5 +59,97 @@ namespace Nebula.Caching.tests.Redis.CacheManager
             mockedIDatabase.Verify(m => m.StringGet("key", It.IsAny<CommandFlags>()), Times.Once);
         }
 
+        [Fact]
+        public void Given_AMethodExecutedWithRedisCacheAttribute_When_CacheDoesNotExist_Then_SetCache()
+        {
+            //Arrange
+            var mockedIDatabase = new Mock<IDatabase>();
+            var key = "someKey";
+            var value = "someValue";
+            var expiration = TimeSpan.FromSeconds(25);
+            mockedIDatabase.Setup(m => m.StringSet(key, value, It.IsAny<TimeSpan?>(), It.IsAny<bool>(), It.IsAny<When>(), It.IsAny<CommandFlags>())).Returns(It.IsAny<bool>());
+            mockedIDatabase.Setup(m => m.KeyExpire(key, expiration, It.IsAny<ExpireWhen>(), It.IsAny<CommandFlags>())).Returns(It.IsAny<bool>());
+            var redisCacheManager = new RedisCacheManager(mockedIDatabase.Object);
+
+
+            //Act
+            redisCacheManager.Set(key, value, expiration);
+
+            //Assert
+            mockedIDatabase.Verify(m => m.StringSet(key, value, It.IsAny<TimeSpan?>(), It.IsAny<bool>(), It.IsAny<When>(), It.IsAny<CommandFlags>()), Times.Once);
+            mockedIDatabase.Verify(m => m.KeyExpire(key, expiration, It.IsAny<ExpireWhen>(), It.IsAny<CommandFlags>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Given_AMethodExecutedWithRedisCacheAttribute_When_CacheDoesExist_Then_ReturnTrueBecauseCacheExistsAsync()
+        {
+            //Arrange
+            var mockedIDatabase = new Mock<IDatabase>();
+            mockedIDatabase.Setup(m => m.StringGetAsync("key", It.IsAny<CommandFlags>())).ReturnsAsync("value");
+            var redisCacheManager = new RedisCacheManager(mockedIDatabase.Object);
+            var expectedCacheExistence = true;
+
+            //Act
+            var cacheExists = await redisCacheManager.CacheExistsAsync("key");
+
+            //Assert
+            Assert.Equal(expectedCacheExistence, cacheExists);
+            mockedIDatabase.Verify(m => m.StringGetAsync("key", It.IsAny<CommandFlags>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Given_AMethodExecutedWithRedisCacheAttribute_When_CacheDoesNotExist_Then_ReturnFalseBecauseCacheDoesNotExistAsync()
+        {
+            //Arrange
+            var mockedIDatabase = new Mock<IDatabase>();
+            mockedIDatabase.Setup(m => m.StringGetAsync("key", It.IsAny<CommandFlags>())).ReturnsAsync(RedisValue.Null);
+            var redisCacheManager = new RedisCacheManager(mockedIDatabase.Object);
+            var expectedCacheExistence = false;
+
+            //Act
+            var cacheExists = await redisCacheManager.CacheExistsAsync("key");
+
+            //Assert
+            Assert.Equal(expectedCacheExistence, cacheExists);
+            mockedIDatabase.Verify(m => m.StringGetAsync("key", It.IsAny<CommandFlags>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Given_AMethodExecutedWithRedisCacheAttribute_When_CacheExistsAndWillBeRetrieved_Then_CacheValueWillBeReturnedAsync()
+        {
+            //Arrange
+            var mockedIDatabase = new Mock<IDatabase>();
+            mockedIDatabase.Setup(m => m.StringGetAsync("key", It.IsAny<CommandFlags>())).ReturnsAsync("value");
+            var redisCacheManager = new RedisCacheManager(mockedIDatabase.Object);
+            var expectedCacheValue = "value";
+
+            //Act
+            var cacheValue = await redisCacheManager.GetAsync("key");
+
+            //Assert
+            Assert.Equal(expectedCacheValue, cacheValue);
+            mockedIDatabase.Verify(m => m.StringGetAsync("key", It.IsAny<CommandFlags>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Given_AMethodExecutedWithRedisCacheAttribute_When_CacheDoesNotExist_Then_SetCacheAsync()
+        {
+            //Arrange
+            var mockedIDatabase = new Mock<IDatabase>();
+            var key = "someKey";
+            var value = "someValue";
+            var expiration = TimeSpan.FromSeconds(25);
+            mockedIDatabase.Setup(m => m.StringSetAsync(key, value, It.IsAny<TimeSpan?>(), It.IsAny<bool>(), It.IsAny<When>(), It.IsAny<CommandFlags>())).ReturnsAsync(It.IsAny<bool>());
+            mockedIDatabase.Setup(m => m.KeyExpireAsync(key, expiration, It.IsAny<ExpireWhen>(), It.IsAny<CommandFlags>())).ReturnsAsync(It.IsAny<bool>());
+            var redisCacheManager = new RedisCacheManager(mockedIDatabase.Object);
+
+
+            //Act
+            redisCacheManager.SetAsync(key, value, expiration);
+
+            //Assert
+            mockedIDatabase.Verify(m => m.StringSetAsync(key, value, It.IsAny<TimeSpan?>(), It.IsAny<bool>(), It.IsAny<When>(), It.IsAny<CommandFlags>()), Times.Once);
+            mockedIDatabase.Verify(m => m.KeyExpireAsync(key, expiration, It.IsAny<ExpireWhen>(), It.IsAny<CommandFlags>()), Times.Once);
+        }
     }
 }
